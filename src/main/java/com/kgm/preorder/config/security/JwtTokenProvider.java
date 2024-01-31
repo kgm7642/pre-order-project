97e367be98bcc950da16c31f309a9b27f4498f61
KyungMin <kgm7642@gmail.com>
2024-01-31 23:23:06 +0900

    jwt 1차 작성 완료

---
+++ src/main/java/com/kgm/preorder/config/security/JwtTokenProvider.java
@@ -0,0 +1,88 @@
+package com.kgm.preorder.config.security;
+
+import com.kgm.preorder.service.CustomUserDetailService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+    private String secret;
+    private final long tokenValidTime;
+    private final CustomUserDetailService userDetailsService;
+
+    // 생성자
+    public JwtTokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.token-valid-time}") long tokenValidTime,
+            CustomUserDetailService userDetailsService
+    ) {
+        this.secret = Base64.getEncoder().encodeToString(secret.getBytes());
+        this.tokenValidTime = tokenValidTime;
+        this.userDetailsService = userDetailsService;
+    }
+
+    // 객체 초기화, secretKey를 Base64로 인코딩한다.
+    @PostConstruct
+    protected void init() {
+        secret = Base64.getEncoder().encodeToString(secret.getBytes());
+    }
+
+    // JWT 토큰 생성
+    public String createToken(String userPk, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(userPk);
+        claims.put("roles", roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + tokenValidTime);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    // JWT 토큰에서 인증 정보 조회
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    // 토큰에서 회원 정보 추출
+    public String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    // Request의 Header에서 token 값을 가져옵니다. "Authorization" : "TOKEN값'
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("Authorization");
+    }
+
+    // 토큰의 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
--- src/main/java/com/kgm/preorder/entity/Role.java
+++ src/main/java/com/kgm/preorder/entity/Role.java
@@ -1,5 +1,5 @@
 package com.kgm.preorder.entity;
 
 public enum Role {
-    ROLE_MEMBER, ROLE_ADMIN
+    MEMBER, ADMIN
 }
