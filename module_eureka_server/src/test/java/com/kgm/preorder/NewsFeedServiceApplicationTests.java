dfe871312fa73463cb0c62e0a1378d87951eb9b5
KyungMin <kgm7642@gmail.com>
2024-02-18 11:00:06 +0900

    docker compose 수정

---
+++ module_eureka_server/src/test/java/com/kgm/preorder/NewsFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgm.preorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsFeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
